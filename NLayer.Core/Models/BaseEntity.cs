7d4ee4f5b58abc544115efa9cbb1292b5ad282e7
emre5 <emre5@KARACHAI>
2023-06-07 11:46:59 +0300

    Add project files.

---
+++ NLayer.Core/Models/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace NLayer.Core.Models
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CrearedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
